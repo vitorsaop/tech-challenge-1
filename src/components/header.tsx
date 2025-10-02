427447d3166ab0c1c509b39caa31495523b7316d
Leandro de Freitas Rodrigues <leandrodfreitas@gmail.com>
2025-10-02 10:14:26 -0300

    Incluindo imagens no projeto

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,7 @@ export function Header() {
   return (
     <header className="flex items-center sticky top-0 z-50 bg-[#000] shadow-sm h-22 text-white">
       <div className="container mx-auto">
-        <div className="flex items-center justify-between">
-          
+        <div className="flex items-center justify-between">          
           <div className="items-center grid grid-cols-1 gap-16 md:grid-cols-2">
             <Link href="/" aria-label="ByteBank - Página Inicial">
               <Image src={"/logo-green.svg"} alt="Logo ByteBank" width={200} height={32} />
@@ -16,49 +15,15 @@ export function Header() {
               <Link href={"#"} className="text-[#47A138] text-[23px] font-medium hover:underline">Serviços</Link>
             </nav>            
           </div>
-          <div className='grid grid-cols-1 gap-5 md:grid-cols-2'>
-            
-            <Link className="
-              inline-flex 
-              justify-center
-              items-center
-              h-12 
-              text-center 
-              rounded-[8px] 
-              bg-[#47a138]
-              hover:bg-[#FFF] 
-              min-w-[196px]
-              text-[17px]
-              font-medium
-              hover:text-[#59b449]
-              transition duration-300
-            "
-            href={"#"} aria-label="Abrir minha conta">
+          <div className='grid grid-cols-1 gap-5 md:grid-cols-2'>            
+            <Link href={"#"} 
+              className="inline-flex justify-center items-center h-12 text-center rounded-[8px] bg-[#47a138] hover:bg-[#FFF] min-w-[196px] text-[17px] hover:text-[#59b449] transition duration-300" aria-label="Abrir minha conta">
               Abrir minha conta
-            </Link>
-            
-            <Link className="
-              inline-flex 
-              justify-center
-              items-center 
-              h-12
-              text-center 
-              rounded-[8px] 
-              bg-transparent
-              hover:bg-[#47a138] 
-              border-[2px] 
-              border-[#47a138] 
-              min-w-[196px] 
-              text-[#47a138]
-              hover:text-[#FFF]
-              text-[17px]
-              font-medium
-              transition duration-300
-            " 
-            href={"#"} aria-label="Já tenho conta">
-              Já tenho conta
             </Link>            
-
+            <Link href={"#"} 
+              className="inline-flex justify-center items-center h-12 text-center rounded-[8px] bg-transparent hover:bg-[#47a138] border-[2px] border-[#47a138] min-w-[196px] text-[#47a138] hover:text-[#FFF] text-[17px] transition duration-300" aria-label="Já tenho conta">
+              Já tenho conta
+            </Link>
           </div>
         </div>
       </div>
