bf2680e5fd0c3103f7bb9fd77babc496cfe28e8c
Leandro de Freitas Rodrigues <leandrodfreitas@gmail.com>
2025-10-02 11:01:17 -0300

    Criando página Services

---
+++ src/components/footer-services.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export function FooterServices() {
+  return(
+    <div style={{border: "3px solid green"}}>
+      <div className="container mx-auto">
+        Footer Services
+      </div> 
+    </div>
+  )
+}
---
+++ src/components/header-services.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export function HeaderServices() {
+  return(
+    <div style={{border: "3px solid red"}}>
+      <div className="container mx-auto">
+        Header Services
+      </div>      
+    </div>
+  )
+}
